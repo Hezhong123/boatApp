8dcccbb04f1d4c4e32190f1d0010ed2fcd1c165c
hez <626815494@qq.com>
2022-12-19 10:31:50 +0800

    App设置

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, useColorScheme, View} from 'react-native';
+import {StatusBar, StyleSheet, Text, useColorScheme, View} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
@@ -10,6 +10,8 @@ import {Im} from "./src/views/im";
 import {Me} from "./src/views/me";
 import {Add} from "./src/views/add";
 import {Login} from "./src/views/login";
+import {Adds} from "./src/views/adds";
+import {Ticket} from "./src/views/ticket";
 
 const Stack = createNativeStackNavigator();
 
@@ -17,17 +19,22 @@ export default function App() {
     const colorScheme = useColorScheme();
     const C1 = colorScheme == 'light' ? styles.lightC1 : styles.darkC1
     const scheme = colorScheme == 'light' ?  'black' :'white'
+    const Bar = colorScheme == 'light'?'dark-content':'light-content'
+    const BarColor = colorScheme == 'light'?'#F2F2F2':'#2B3140'
 
     return (
         <NavigationContainer  >
+            <StatusBar barStyle={Bar} backgroundColor={BarColor} />
             <Stack.Navigator
                 screenOptions={{
                     headerTintColor: scheme,
                     headerStyle: C1 }}>
-                <Stack.Screen name="List" component={List}/>
+                <Stack.Screen name="List" component={List} options={{title:"小船Im"}} />
                 <Stack.Screen name="Im" component={Im}/>
                 <Stack.Screen name="Me" component={Me}/>
                 <Stack.Screen name="Add" component={Add}/>
+                <Stack.Screen name="Adds" component={Adds}/>
+                <Stack.Screen name="Ticket" options={{title:"解锁全功能"}} component={Ticket}/>
                 <Stack.Screen name="Login" component={Login}/>
             </Stack.Navigator>
         </NavigationContainer>
