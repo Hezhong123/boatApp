7102c4f5a6eea642cf372493779565a14c287b53
hez <626815494@qq.com>
2022-12-19 10:31:05 +0800

    按钮组件

--- src/component/btn.js
+++ src/component/btn.js
@@ -1,5 +1,8 @@
-import {Text, TouchableOpacity, useColorScheme, View} from "react-native";
+import {Alert, Text, TouchableOpacity, useColorScheme, View} from "react-native";
 import {styles} from "../css";
+import * as React from "react";
+import {useRef} from "react";
+import * as Clipboard from "expo-clipboard";
 
 export function Btn(props){
     const colorScheme = useColorScheme();
@@ -11,4 +14,32 @@ export function Btn(props){
         onPress={press}>
         <Text style={{fontSize:fs}}>{text}</Text>
     </TouchableOpacity>
+}
+
+
+
+export function TextClick(props){
+
+    //点击，双击，长按，
+    const {click=()=>{},doubleClick=()=>{},press=()=>{},slot} = props
+    const backCount = useRef(0)
+    const backTimer = useRef(null)
+    return<TouchableOpacity onPress={() => {
+        backCount.current++
+        if (backCount.current == 2) {
+            doubleClick()  //双击
+            clearTimeout(backTimer.current)
+        }else if(backCount.current == 1){
+            click() //点击
+        }else {
+            backTimer.current = setTimeout(() => {
+                backCount.current = 0
+            }, 300)
+        }
+    }} onLongPress={()=>press()}>
+        {slot}
+    </TouchableOpacity>
+
+
+
 }
