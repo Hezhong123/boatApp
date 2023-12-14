772edb69971d80c67721bdf38d80ad07608994c0
hez <2022hez@gmail.com>
2023-12-14 11:01:26 +0800

    激活码

--- pages/key.js
+++ pages/key.js
@@ -1,6 +1,7 @@
 import {View,Text} from "react-native";
 import React from "react";
-export function imKey(){
+
+export function key() {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>激活码</Text>
