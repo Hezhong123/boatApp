03e11c2cb86a5dc4a43dd2139ff0df8ebe15de27
hez <2022hez@gmail.com>
2023-12-14 10:54:07 +0800

    联系人列表

---
+++ pages/list.js
@@ -0,0 +1,10 @@
+import {View,Text} from "react-native";
+import React from "react";
+
+export function List() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>联系人列表</Text>
+        </View>
+    );
+}
