4d8a886820f7d9aff153522bb8adb732e9bd3838
hez <2022hez@gmail.com>
2023-12-14 10:53:49 +0800

    对话页面

---
+++ pages/im.js
@@ -0,0 +1,10 @@
+import {View,Text} from "react-native";
+import React from "react";
+
+export function Im() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>对话列表</Text>
+        </View>
+    );
+}
