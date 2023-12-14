bd99d2e96ef8c3abebe8fc0fafece15c5324cfb7
hez <2022hez@gmail.com>
2023-12-14 10:53:27 +0800

    添加联系人

---
+++ pages/addIm.js
@@ -0,0 +1,9 @@
+import {View,Text} from "react-native";
+import React from "react";
+export function addIm(){
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>添加联系人</Text>
+        </View>
+    );
+}
