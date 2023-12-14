6605991623f1b39bc9c79f6253335d7838e27214
hez <2022hez@gmail.com>
2023-12-14 10:53:37 +0800

    获取验证码

---
+++ pages/authCode.js
@@ -0,0 +1,9 @@
+import {View,Text} from "react-native";
+import React from "react";
+export function authCode(){
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>电话验证码</Text>
+        </View>
+    );
+}
