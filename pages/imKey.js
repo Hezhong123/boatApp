3d2ea9d1bea8dd1363b66fef135081c69d5f81f7
hez <2022hez@gmail.com>
2023-12-14 10:53:58 +0800

    激活码页面

---
+++ pages/imKey.js
@@ -0,0 +1,9 @@
+import {View,Text} from "react-native";
+import React from "react";
+export function imKey(){
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>激活码</Text>
+        </View>
+    );
+}
