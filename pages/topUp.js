24194a0240bf13281751482d8f1f1316e60174de
hez <2022hez@gmail.com>
2023-12-14 10:54:33 +0800

    充值页面

---
+++ pages/topUp.js
@@ -0,0 +1,9 @@
+import {View,Text} from "react-native";
+import React from "react";
+export function topUp(){
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>充值</Text>
+        </View>
+    );
+}
