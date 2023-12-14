2cd09b8a67bcbaf81695aca48f506c7e744f1920
hez <2022hez@gmail.com>
2023-12-14 10:54:23 +0800

    查询联系人

---
+++ pages/queryIm.js
@@ -0,0 +1,9 @@
+import {View,Text} from "react-native";
+import React from "react";
+export function queryIm(){
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>查找联系人</Text>
+        </View>
+    );
+}
