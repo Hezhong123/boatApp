878475f2d2c07fe28b6125d3563a6a2721f4f936
hez <2022hez@gmail.com>
2023-12-14 10:54:44 +0800

    充值记录

---
+++ pages/upList.js
@@ -0,0 +1,9 @@
+import {View,Text} from "react-native";
+import React from "react";
+export function upList(){
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>充值记录</Text>
+        </View>
+    );
+}
