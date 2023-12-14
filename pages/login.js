cd95dc243cb01a344e246ca5c28b249af7e3c11c
hez <2022hez@gmail.com>
2023-12-14 10:54:14 +0800

    登陆页面

---
+++ pages/login.js
@@ -0,0 +1,10 @@
+import {View,Text} from "react-native";
+import React from "react";
+
+export function Login() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>登陆</Text>
+        </View>
+    );
+}
