7f13603181fb7de207e2b1d8ab2482fa75685d4d
hez <2022hez@gmail.com>
2025-04-19 00:16:22 +0800

    联系人列表跟新

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,6 @@
+import {userMsg,Ims} from '../../models/index'
+import io from '../../utils/weapp.socket.io'
+
 
 Page({
 
@@ -6,7 +9,11 @@ Page({
    */
   data: {
     modelA:false,
-    userImg:'https://img.freepik.com/free-photo/asian-man-wearing-glasses-portrait-smiling-face-close-up_53876-139746.jpg'
+    ims:[], //信道
+    _ims:[], //信道下标
+    user:{
+        avatarUrl:''
+    }
   },
 
   childCatchTap:function(e){
@@ -26,12 +33,47 @@ Page({
       url: `/pages/${page}/${page}`,
     })
   },
+
+//   进入信道
+  navIms: function(e){
+      console.log(e);
+    let ims = e.currentTarget.dataset.ims
+    let title = e.currentTarget.dataset.title
+    wx.navigateTo({
+        url: `/pages/im/im?im=${ims}&title=${title}`,
+    })
+  },
+  
+
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
+   onLoad: async function (options) {
+       let ims = await Ims()
+       let _ims = []  //信道下标
+       ims.map(item=>{
+        _ims.push(item._id)
+       })
+       let user = await userMsg()
+       console.log('信道列表',ims);
+       const socket = io('http://192.168.1.6:3000');
+        socket.on(user.id, (data) => {
+            const index = _ims.findIndex(element => element === data._id);
+            ims.splice(index,1,data)
+            this.setData({
+                ims:ims
+            })
+            console.log(index,_ims,data._id);
+        });
+        this.socket = socket
+        this.setData({
+            user: user,
+            ims: ims,
+            _ims:_ims
+        })
   },
+
+ 
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -51,14 +93,15 @@ Page({
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+    this.socket.disconnect(); // 断开连接
+    console.log('ims 断开连接');
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+   
   },
 
   /**
