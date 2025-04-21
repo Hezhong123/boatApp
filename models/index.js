7cd412774e23ab08ba6b34e7612a28684271b8bb
hez <2022hez@gmail.com>
2025-04-21 17:00:26 +0800

    各种模型

--- models/index.js
+++ models/index.js
@@ -26,11 +26,34 @@ export const Ims = () => {
     })
 }
 
+//信道ID
+export const ImsId = (id) => {
+    return request({
+        url:`/ims/id/${id}`,
+        method: 'GET'
+    })
+}
 //信道消息
 export const imMsg =  (im) => {
     return request({
         url:`/msg/${im}`,
         method: 'GET'
+    })
+}
+
+//跟新信道时间
+export const msgTime =  (id) => {
+    return request({
+        url:`/msg/time/${id}`,
+        method: 'PUT'
+    })
+}
+
+//未读消息跟新
+export const imsUnread = (im) =>{
+    return request({
+        url:`/ims/unread/${im}`,
+        method: 'PUT'
     })
 }
 
@@ -40,5 +63,29 @@ export const cosToken = (key,path) =>{
         url:'/cos/authorizationWeCat',
         method: 'POST',
         data:{key,path}
+    })
+}
+
+// 订单查询
+export const orderLi = ()=>{
+    return request({
+        url:`/orders`,
+        method: 'GET'
+    })
+}
+
+//激活码查询
+export const getCode =(key)=>{
+    return request({
+        url:`/orders/code/${key}`,
+        method: 'GET'
+    })
+}
+
+// 激活码激活
+export const postCode =(key)=>{
+    return request({
+        url:`/orders/code/${key}`,
+        method: 'POST'
     })
 }
